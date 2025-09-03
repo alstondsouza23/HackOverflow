71e2b18183d12aec1d79b0b8e79ef72aea0f8f1b
alstondsouza23 <88037700+alstondsouza23@users.noreply.github.com>
2025-09-04 00:42:12 +0530

    Update page.jsx

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { app } from '@/app/firebase/config';
-
+//
 export default function SignInPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -104,4 +104,5 @@ export default function SignInPage() {
       </main>
     </div>
   );
+
 }
